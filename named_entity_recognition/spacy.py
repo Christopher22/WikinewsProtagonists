b3b2b0926663d8d8e1f3086d1181f05b7ba46a74
Christopher Gundler <c.gundler@mail.de>
2019-02-23 14:39:12 +0100

    Optimize SpaCy extraction

--- named_entity_recognition/spacy.py
+++ named_entity_recognition/spacy.py
@@ -1,4 +1,4 @@
-from typing import Sequence, Mapping
+from typing import Sequence, Mapping, Optional
 from collections import defaultdict
 
 import spacy
@@ -12,21 +12,40 @@ class Spacy(NamedEntityRecognition):
         nlp = spacy.load("en_core_web_sm")
 
         article_entities = [
-            frozenset(
-                entity.lemma_.split()[
-                    -1
-                ]  # Extract the surname, if multiple name parts are available
-                for entity in article.ents
-                if entity.label_ == "PERSON" and len(entity.lemma_) > 0
-            )
+            frozenset(Spacy.__get_persons(article))
             for article in nlp.pipe(
-                (str(article.content) for article in articles), n_threads=8, batch_size=32
+                (str(article.content) for article in articles),
+                n_threads=8,
+                batch_size=32,
             )
         ]
 
         entities = defaultdict(list)
         for article, local_entities in zip(articles, article_entities):
             for entity in local_entities:
-                entities[entity.capitalize()].append(article)
+                entities[entity].append(article)
 
         return entities
+
+    @staticmethod
+    def __get_persons(document) -> Optional[str]:
+        for entity in document.ents:
+            # Filter entities not of further interest
+            if entity.label_ != "PERSON" or len(entity.lemma_) == 0:
+                continue
+
+            # Get multiple parts of the name
+            name_parts = entity.lemma_.split()
+
+            # Remove potential "'s" at the end of names
+            if len(name_parts[-1]) <= 3:
+                name_parts = name_parts[:-1]
+
+            # Still a valid entity, but not of further interest.
+            if len(name_parts) < 2:
+                continue
+
+            # Remove possible mittle names and return only given name and surname
+            yield "{} {}".format(
+                name_parts[0].capitalize(), name_parts[-1].capitalize()
+            )
